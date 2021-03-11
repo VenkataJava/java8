ac3a0ccb25bbbd9113e7f53297970ee96c6624ac
VenkataJava <79977837+VenkataJava@users.noreply.github.com>
2021-03-11 17:27:50 +0530

    Update GetCountOfWordInString.java

--- src/com/ups/test/java8/GetCountOfWordInString.java
+++ src/com/ups/test/java8/GetCountOfWordInString.java
@@ -1,6 +1,8 @@
 package com.ups.test.java8;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -12,16 +14,23 @@ public class GetCountOfWordInString {
 		// TODO Auto-generated method stub
 
 		String str = "aa bb cc    kk gg ll aa    kk";
-		System.out.println(str.split("\\s+").length);
+		System.out.println("word count using regular expression::--- " + str.split("\\s+").length);
 
 		int count = wordCount(str);
-		System.out.println(count);
+		System.out.println("word count using conditions::-- " + count);
 
-		List<String> wordsList = Arrays.asList("hello", "bye", "ciao", "bye", "ciao");
+		List<String> wordsList = Arrays.asList("Apple", "Apple", "Apple", "Apple", "Orange", "Orange", "Orange",
+				"Mango", "Mango", "PineApple");
 		Map<String, Long> wordsCount = wordsList.stream()
 				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-		System.out.println(wordsCount);
-		System.out.println(wordCount(str));
+
+		System.out.println("unsorted by values in descending order:" + wordsCount);
+
+		Map<String, Long> sortedByValue = wordsCount.entrySet().stream()
+				.sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).collect(Collectors.toMap(
+						Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+		System.out.println("sorting by values in descending order:" + sortedByValue);
 	}
 
 	public static int wordCount(String str) {
